f5b22bb4da1354c428d305da78a49ebe8518893f
simasch <simon@72.services>
2018-02-22 11:45:28 +0100

    do lookup instead of injecting the EJB

--- sqstore-web/src/main/java/store/customer/boundry/CustomerScheduledExecutor.java
+++ sqstore-web/src/main/java/store/customer/boundry/CustomerScheduledExecutor.java
@@ -1,5 +1,6 @@
 package store.customer.boundry;
 
+import org.apache.log4j.Logger;
 import store.customer.control.CustomerService;
 
 import javax.annotation.PostConstruct;
@@ -8,21 +9,33 @@ import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.enterprise.concurrent.ManagedScheduledExecutorService;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.util.concurrent.TimeUnit;
 
 @Startup
 @Singleton
 public class CustomerScheduledExecutor {
 
+    private final static Logger LOGGER = Logger.getLogger(CustomerService.class);
+
     @Resource
     private ManagedScheduledExecutorService managedScheduledExecutorService;
 
-    @EJB
-    private CustomerService customerService;
-
     @PostConstruct
     public void init() {
-        managedScheduledExecutorService.scheduleAtFixedRate(customerService::tick, 10, 10, TimeUnit.SECONDS);
+        managedScheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    CustomerService customerService =
+                            InitialContext.doLookup("java:module/" + CustomerService.class.getSimpleName());
+                    customerService.tick();
+                } catch (NamingException e) {
+                    LOGGER.warn(e.getMessage(), e);
+                }
+            }
+        }, 10, 10, TimeUnit.SECONDS);
     }
 
 }
